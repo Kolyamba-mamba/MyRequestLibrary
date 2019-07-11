3c86f3b31afb51f19cf4415d43fbd7ef1fc5c04f
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2019-07-11 17:34:53 +0400

    added sample request

--- app/src/main/java/com/example/pidsrequest/MainActivity.kt
+++ app/src/main/java/com/example/pidsrequest/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.pidsrequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.corenetworkapipid.Factory
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+const val BASE_URL = "https://child-camp2.herokuapp.com/api/v1/"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Example of working with queries
+        btnRequest.setOnClickListener(){
+            Factory.sharedUrl = BASE_URL
+            val router = Factory.shared
+
+            CoroutineScope(Dispatchers.IO).launch {
+                var responseAll = router.getAllPids()
+                var responseInId = router.getPidById(1)
+                println(responseAll)
+                println(responseInId)
+
+                withContext(Dispatchers.Main) {
+                    tvResponse.setText("$responseAll\n")
+                }
+            }
+        }
     }
 }
